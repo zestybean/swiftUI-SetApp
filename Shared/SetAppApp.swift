e4802b143aff7b7bc81c20edd1619a594befdaf0
Brando Lugo <brando.lugo@gmail.com>
2020-12-09 18:13:41 -0600

    Initial Commit

---
+++ Shared/SetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetAppApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct SetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
